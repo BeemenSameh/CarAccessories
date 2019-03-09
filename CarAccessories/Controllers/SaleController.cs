9f28fc09d002e777e367d7ff91384a3ffa435cc6
RetaYousri <48245946+RetaYousri@users.noreply.github.com>
2019-03-09 12:42:36 +0200

    sale

--- CarAccessories/Controllers/SaleController.cs
+++ CarAccessories/Controllers/SaleController.cs
@@ -1,36 +1,52 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
-//using CarAccessories.Models;
-////using CarAccessories.Models.ViewModels;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarAccessories.Models;
+using CarAccessories.Models.ViewModel;
+//using CarAccessories.Models.ViewModels;
 
-//namespace WebApplication1.Controllers
-//{
-//    public class SaleController : Controller
-//    {
-//        ApplicationDbContext db = new ApplicationDbContext();
-//        // GET: Sale
-//        public ActionResult Index()
-//        {
+namespace WebApplication1.Controllers
+{
+    public class SaleController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Sale
+        public ActionResult Index()
+        {
 
-//            List<Product> SaleProductsList = db.Products.Where(i => i.Sale_price != 0).ToList();
-              
-//            return View(SaleProductsList);
-//        }
+            BrandsProducts b = new BrandsProducts
+            {
+                Brands = db.Brands.ToList(),
+                VendorProducts = db.VendorProducts.Where(p=>p.Sale_price!=0).ToList(),
+                CategoriesList = db.Categories.ToList()
+            };
+            foreach (var vendorProduct in b.VendorProducts)
+            {
+                db.Entry(vendorProduct).Reference(p => p.Product).Load();
+                db.Entry(vendorProduct).Reference(v => v.Vendor).Load();
+            }
+           
+            return View(b);
+        }
 
-//        //public ActionResult GetProductsByCatId(int Category_ID)
-//        //{
-//        //    List<Product> ProductsList = db.Products.Where(i => i.Category.ID == Category_ID).Where(p=>p.Sale_price!=0).ToList();
-//        //    return PartialView("_GetProdByCatIdPartialView", ProductsList);
+        //public ActionResult GetProductsByCatId(int Category_ID)
+        //{
+        //    List<Product> ProductsList = db.Products.Where(i => i.Category.ID == Category_ID).Where(p=>p.Sale_price!=0).ToList();
+        //    return PartialView("_GetProdByCatIdPartialView", ProductsList);
 
-//        //}
+        //}
 
-//        public ActionResult GetAllProducts()
-//        {
-//            List<Product> ProductList = db.Products.Where(p=>p.Sale_price!=0).ToList();
-//            return PartialView("_AllProductsPartialView", ProductList);
-//        }
-//    }
-//}
+        public ActionResult GetAllProducts()
+        {
+            List<VendorProduct> AllVendorProductsList = db.VendorProducts.Where(p=>p.Sale_price!=0).ToList();
+            foreach (var i in AllVendorProductsList)
+            {
+                db.Entry(i).Reference(p => p.Product).Load();
+                db.Entry(i).Reference(p => p.Vendor).Load();
+            }
+            return PartialView("_AllProductsPartialView", AllVendorProductsList);
+        }
+    }
+}
