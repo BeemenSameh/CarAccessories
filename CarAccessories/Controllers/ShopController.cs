c100e80a1af0adf3ae925fe27c6af075dcf03883
BeemenSameh <bemaneg63@gmail.com>
2019-03-12 12:45:06 +0200

    Edit Add to cart

--- CarAccessories/Controllers/ShopController.cs
+++ CarAccessories/Controllers/ShopController.cs
@@ -30,17 +30,7 @@ namespace CarAccessories.Controllers
             }
             return View(b);
         }
-
-        //public ActionResult GetProductsByModelId(int Model_ID)
-        //{
-        //    //{  ? p.Price == f : p.Sale_price == f}
-        //    List<Product> ProductsList = db.Products.Where(i => i.Model.ID == Model_ID).ToList();
-
-
-        //    return PartialView("_GetProdByCatIdPartialView", ProductsList);
-
-        //}
-
+        
         public ActionResult GetAllProducts()
         {
             List<VendorProduct> AllVendorProductsList = db.VendorProducts.ToList();
@@ -52,77 +42,62 @@ namespace CarAccessories.Controllers
             return PartialView("_AllProductsPartialView", AllVendorProductsList);
         }
 
+        public ActionResult addCartDetailsToDataBase(string ProductName)
+        {
+            ClaimsIdentity claimsIdentity = User.Identity as ClaimsIdentity;
+            if (claimsIdentity != null)
+            {
+                var userIdClaim = claimsIdentity.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+                if (userIdClaim != null)
+                {
+                    var userIdValue = userIdClaim.Value;
+                    Customer c = db.Customers.Where(i => i.ID == userIdValue).FirstOrDefault();
+                    db.Entry(c).Collection(o => o.Orders).Load();
+                    foreach (var o in c.Orders)
+                    {
+                        if (o.Isbuy == false)
+                        {
+                            Product product = db.Products.Where(p => p.Name == ProductName).FirstOrDefault();
+                            OrderDetails od = new OrderDetails
+                            {
+                                Price = db.VendorProducts.Where(vp => vp.Product.Equals(product)).Select(vp => vp.Price).FirstOrDefault(),
+                                Quantity = 0
+                            };
+                            od.VendorProduct.Product = db.Products.Where(p => p.Name == ProductName).FirstOrDefault();
+                            o.OrderDetails.Add(od);
+                            db.SaveChanges();
+                            break;
+                        }
+                        else
+                        {
+                            Order order = new Order();
+                            order.Isbuy = false;
+                            order.OrderDetails = new List<OrderDetails>();
+                            Product product = db.Products.Where(p => p.Name == ProductName).FirstOrDefault();
+                            OrderDetails od = new OrderDetails
+                            {
+                                Price = db.VendorProducts.Where(vp => vp.Product.Equals(product)).Select(vp => vp.Price).FirstOrDefault(),
+                                Quantity = 0
+                            };
+                            od.VendorProduct.Product = db.Products.Where(p => p.Name == ProductName).FirstOrDefault();
+                            o.OrderDetails.Add(od);
+                            db.SaveChanges();
+                            break;
+                        }
+                    }
+                    return View();
+                }
+                else
+                {
+                    return Redirect("/Account/Login");
+                }
 
-        //public ActionResult Sale()
-        //{
-        //    CategoryProducts Shop = new CategoryProducts
-        //    {
-        //        Products = db.Products.Where(i => i.Sale_price != 0).ToList(),
-        //        Categories = db.Categories.ToList(),
-        //    };
-        //    return View("Index", Shop);
-        //}
+            }
+            else
+            {
+                return Redirect("/Account/Register");
+            }
 
-        //public ActionResult addCartDetailsToDataBase(string ProductName)
-        //{
-        //    var claimsIdentity = User.Identity as ClaimsIdentity;
-        //    if (claimsIdentity != null)
-        //    {
-
-        //        var userIdClaim = claimsIdentity.Claims
-        //            .FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
-
-        //        if (userIdClaim != null)
-        //        {
-        //            var userIdValue = userIdClaim.Value;
-        //            Customer c = db.Customers.Where(i => i.ID == userIdValue).FirstOrDefault();
-
-        //            db.Entry(c).Collection(o => o.Orders).Load();
-        //            foreach (var o in c.Orders)
-        //            {
-        //                if (o.Isbuy == false)
-        //                {
-        //                    OrderDetails od = new OrderDetails();
-        //                    od.Price = db.Products.Where(p => p.Name == ProductName).Select(p => p.Price).FirstOrDefault();
-        //                    od.VendorProduct.Product = db.Products.Where(p => p.Name == ProductName).FirstOrDefault();
-
-        //                    break;
-        //                    // o.OrderDetails.Add();
-        //                }
-        //            }
-
-        //            //c.IsPaid = false;
-        //            db.Carts.Add(c);
-        //            db.SaveChanges();
-        //            return View();
-        //        }
-        //        else
-        //        {
-        //            return Redirect("/Account/Login");
-
-        //        }
-
-        //    }
-        //    else
-        //    {
-        //        return Redirect("/Account/Register");
-        //    }
-
-        //}
-
-        //public ActionResult ShowProductDetails(int id)
-        //{
-
-        //    Product p = db.Products.Where(i => i.ID == id).FirstOrDefault();
-
-        //    return View("_ProductDetailsView", p);
-        //}
-
-        //public ActionResult GetProductsByCategoryId(int Cat_ID)
-        //{
-        //    List<Product> ProductsList = db.Categories.Where(i => i.ID == Cat_ID).Select(p => p.Products).FirstOrDefault().ToList();
-        //    return PartialView("_GetProdByCatIdPartialView", ProductsList);
-        //}
-
+        }
     }
 }
