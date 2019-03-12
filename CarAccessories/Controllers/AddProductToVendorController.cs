1cdb65d82fd31386c6d876f3ed4bf202c8a7d2d1
Mahmoud Abdelnaser <34347417+Mahmoud3060@users.noreply.github.com>
2019-03-12 15:31:10 +0200

    AddProductToVendor

---
+++ CarAccessories/Controllers/AddProductToVendorController.cs
@@ -0,0 +1,20 @@
+﻿using CarAccessories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarAccessories.Controllers
+{
+    public class AddProductToVendorController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext(); 
+        // GET: VendorProduct
+        public ActionResult Index()
+        {
+            var prod = db.Products.ToList();
+            return View(prod);
+        }
+    }
+}
