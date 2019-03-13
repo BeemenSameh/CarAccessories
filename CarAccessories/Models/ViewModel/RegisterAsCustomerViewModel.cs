192cd01a5271c7097e272cdcf47924770da79c65
RetaYousri <48245946+RetaYousri@users.noreply.github.com>
2019-03-13 02:57:14 +0200

    Vendor&customerRegisteration

---
+++ CarAccessories/Models/ViewModel/RegisterAsCustomerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarAccessories.Models.ViewModel
+{
+    public class RegisterAsCustomerViewModel
+    {
+        public string UserId { get; set; }
+        public Customer customer { get; set; }
+
+
+
+    }
+}
