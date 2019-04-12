ccd78e587feda00a091f7e95ec8c768d80676f6d
BeemenSameh <bemaneg+>
2019-04-13 00:02:48 +0200

    Final Commit

--- CarAccessories/App_Start/RouteConfig.cs
+++ CarAccessories/App_Start/RouteConfig.cs
@@ -18,6 +18,8 @@ namespace CarAccessories
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+           
         }
     }
 }
